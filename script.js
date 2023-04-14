1a31953d5f67d96a02ab9c19174beb686871965b
Mamtaprajapati123 <124058185+Mamtaprajapati123@users.noreply.github.com>
2023-04-15 01:10:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+// Get the element with id="level"
+var levelElement = document.getElementById("level");
+
+// Initialize a variable to keep track of the DOM level
+var level = 0;
+
+// Traverse up the DOM tree from the element to the <html> element
+while (levelElement !== null) {
+  level++;
+  levelElement = levelElement.parentNode;
+}
+
+// Print the final answer using alert()
+alert("The level of the element is: " + level);
